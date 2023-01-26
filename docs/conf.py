e2605b928c8d7814cee302c4b58914a1c386b031
Fabian Gothe <fabiangothe@gmail.com>
2023-01-26 14:49:25 +0100

    Update conf.py Extension now "-" instead of "_"

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,6 @@ import sys
 # add example module to the python path
 sys.path.insert(0, os.path.dirname(__file__))
 
-extensions = ["sphinx_sqlalchemy"]
+extensions = ["sphinx-sqlalchemy"]
 
 html_theme = "furo"
