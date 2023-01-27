7a05df7d13db4139874d3d408eb95b237317fd5a
fabiangothe <fabiangothe@gmail.com>
2023-01-27 15:04:25 +0100

    changed path to example in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # add example module to the python path
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 
 extensions = ["sphinx_sqlalchemy"]
 
