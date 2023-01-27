4f81d519cfa684ad837505db44581f9ec0c0e311
fabiangothe <fabiangothe@gmail.com>
2023-01-27 16:50:00 +0100

    changed position of example back and path in config.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 # add example module to the python path
+# ist in GitHub im original so drin, aber geht auch ohne, warum???
 #sys.path.insert(0, os.path.dirname(__file__))
 
-
+#die folgenden Zeilen waren nur zum Ausprobieren
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 #sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath('.')), 'example'))
 #sys.path.insert(0, os.path.dirname(os.path.abspath("../")))
