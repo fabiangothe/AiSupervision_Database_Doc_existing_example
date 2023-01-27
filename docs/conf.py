02cb615c6f6d79bf7a2badc46ae43bbf81c55428
fabiangothe <fabiangothe@gmail.com>
2023-01-27 16:39:32 +0100

    changed position of example and path in config.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,14 +3,16 @@ import os
 import sys
 
 # add example module to the python path
-sys.path.insert(0, os.path.dirname(__file__))
-
-extensions = ["sphinx_sqlalchemy"]
-
-html_theme = "furo"
+#sys.path.insert(0, os.path.dirname(__file__))
 
 
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 #sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath('.')), 'example'))
-#sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../"))
 #sys.path.insert(0, os.path.join(os.path.abspath("../"), 'example'))
+
+
+
+extensions = ["sphinx_sqlalchemy"]
+
+html_theme = "furo"
