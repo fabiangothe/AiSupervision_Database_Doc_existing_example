eadc7b0e90bddef9d198b81e25b8c612b3f877d4
fabiangothe <fabiangothe@gmail.com>
2023-01-27 16:45:43 +0100

    changed position of example and path in config.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ import sys
 
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 #sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath('.')), 'example'))
-sys.path.insert(0, os.path.dirname(os.path.abspath("../")))
+#sys.path.insert(0, os.path.dirname(os.path.abspath("../")))
 #sys.path.insert(0, os.path.join(os.path.abspath("../"), 'example'))
 
 
