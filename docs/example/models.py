ae5b1af64106016f3ae164c659a28f4ab1954697
fabiangothe <fabiangothe@gmail.com>
2023-01-30 18:18:26 +0100

    deleted comments

--- docs/example/models.py
+++ docs/example/models.py
@@ -1,36 +1,3 @@
-'''example for normale SQLAlchemy
-"""Example SQLAlchemy ORM models."""
-from sqlalchemy import CheckConstraint, Column, ForeignKey, UniqueConstraint, orm, types
-
-Base = orm.declarative_base()
-
-
-class User(Base):
-    """A ``user``."""
-
-    __tablename__ = "dbusers"
-    __table_args__ = (UniqueConstraint("first_name", "last_name"),)
-    pk = Column(types.Integer, primary_key=True)
-    first_name = Column(types.String, doc="The name of the user.")
-    last_name = Column(types.String(255), doc="The surname of the user.")
-    dob = Column(types.Date, nullable=False, doc="The date of birth.")
-
-
-class Address(Base):
-    """An address."""
-
-    __tablename__ = "addresses"
-    __table_args__ = (CheckConstraint("number>0", name="check1"),)
-    pk = Column(types.Integer, primary_key=True)
-    number = Column(types.Integer, nullable=False, doc="The number of the address.")
-    postcode = Column(
-        types.String, nullable=False, index=True, doc="The postcode of the address."
-    )
-    user_id = Column(types.Integer, ForeignKey("dbusers.pk"))
-    user = orm.relationship("User")
-'''
-
-
 
 #added myself
 import pymysql
@@ -84,29 +51,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug = True)
-'''
-
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://test_user:1234@localhost/documentation_test_database"
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-
-@app.route("/")
-def index():
-    user = User(name="John Doe", email="johndoe@example.com")
-    db.session.add(user)
-    db.session.commit()
-    return "User added to the database."
-
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
-'''
