3de63ad61bb553a24712faa3ee8e86fca7afab57
fabiangothe <fabiangothe@gmail.com>
2023-01-30 19:06:26 +0100

    changes

--- docs/example/models.py
+++ docs/example/models.py
@@ -17,17 +17,18 @@ db = SQLAlchemy(app)
 
 class students(db.Model):
     id = db.Column('student_id', db.Integer, primary_key = True)
-    name = db.Column(db.String(100), doc = 'test_Doc')
-    city = db.Column(db.String(50))
-    addr = db.Column(db.String(200))
-    pin = db.Column(db.String(10))
+    name = db.Column(db.String(100), doc = "test_Doc")
+    city = db.Column(db.String(50), doc = 'test_Doc')
+    addr = db.Column(db.String(200), doc = 'test_Doc')
+    pin = db.Column(db.String(10), doc = 'test_Doc')
 
+'''
 def __init__(self, name, city, addr,pin):
     self.name = name
     self.city = city
     self.addr = addr
     self.pin = pin
-
+'''
 @app.route('/')
 def show_all():
     return render_template('show_all.html', students = students.query.all() )
